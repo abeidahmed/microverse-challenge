4543500e2fa388019f70dc41a74cc5bb70b7ae3c
abeidahmed <abeidahmed92@gmail.com>
2020-09-02 20:26:41 +0530

    create mobile menu

--- js/index.js
+++ js/index.js
@@ -1,28 +1,17 @@
-// !! IMPORTANT README:
+'use strict';
 
-// You may add additional external JS and CSS as needed to complete the project, however the current external resource MUST remain in place for the tests to work. BABEL must also be left in place. 
+const openBtn = document.querySelector('#open-btn');
+const closeBtn = document.querySelector('#close-btn');
+const mobileMenu = document.querySelector('#mobile-menu');
 
-/***********
-INSTRUCTIONS:
-  - Select the project you would 
-    like to complete from the dropdown 
-    menu.
-  - Click the "RUN TESTS" button to
-    run the tests against the blank 
-    pen.
-  - Click the "TESTS" button to see 
-    the individual test cases. 
-    (should all be failing at first)
-  - Start coding! As you fulfill each
-    test case, you will see them go   
-    from red to green.
-  - As you start to build out your 
-    project, when tests are failing, 
-    you should get helpful errors 
-    along the way!
-    ************/
+openBtn.addEventListener('click', () => {
+  closeBtn.classList.remove('hidden');
+  mobileMenu.classList.remove('hidden');
+  openBtn.classList.add('hidden');
+});
 
-// PLEASE NOTE: Adding global style rules using the * selector, or by adding rules to body {..} or html {..}, or to all elements within body or html, i.e. h1 {..}, has the potential to pollute the test suite's CSS. Try adding: * { color: red }, for a quick example!
-
-// Once you have read the above messages, you can delete all comments. 
-"use strict";
+closeBtn.addEventListener('click', () => {
+  closeBtn.classList.add('hidden');
+  mobileMenu.classList.add('hidden');
+  openBtn.classList.remove('hidden');
+});
